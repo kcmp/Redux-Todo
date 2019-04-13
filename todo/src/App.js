8a88cd86a96451a6dfd6f911dd226ffc0c29d227
unknown <kcmp711@gmail.com>
2019-04-13 13:14:17 -0400

    working Todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+import Form from './components/Form';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>To Do</h1>
+        <Form />
+        <TodoList />
       </div>
     );
   }
 }
-
-export default App;
+ export default App;
